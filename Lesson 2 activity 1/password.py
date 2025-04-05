47c57134c111a0620178987f721238c7e992c171
Ricckyyy709 <Rickytu2008@gmail.com>
2025-04-05 16:48:43 +0300

    eee

---
+++ Lesson 2 activity 1/password.py
@@ -0,0 +1,8 @@
+import random
+import string
+
+def generate_password(length):
+  """Generates a random password of the specified length."""
+  characters = string.ascii_letters + string.digits + string.punctuation
+  password = ''.join(random.choice(characters) for i in range(length))
+  return password
